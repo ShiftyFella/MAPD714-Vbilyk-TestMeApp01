e82a61aa0c8f22ca65589b884123e9ee95083031
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-06 21:46:23 -0400

    Initial Commit

---
+++ Vbilyk-TestMeApp01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vbilyk-TestMeApp01
+//
+//  Created by Viktor Bilyk on 2017-09-06.
+//  Copyright © 2017 WonderLands LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
